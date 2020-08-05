147b709a4bc707f385d206e0fc1937c48654c805
mmcguire91 <mmcguire91@gmail.com>
2020-08-05 17:16:56 -0400

    Built 'add new transaction' container, functionality to store values of textFields

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,9 @@ class Home extends StatelessWidget {
     ),
   ];
 
+  String title;
+  String amount;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -43,58 +46,102 @@ class Home extends StatelessWidget {
           ],
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Container(
               width: double.infinity,
               margin: EdgeInsets.all(15.0),
               child: Card(
                 //Card is a freestyle container
-                color: Colors.lightBlue[300],
+                color: Color(0xff01655e),
                 child: Text('Chart'),
                 elevation: 5.0,
+              ),
+            ),
+            Card(
+              elevation: 5.0,
+              child: Container(
+                width: double.infinity,
+                margin: EdgeInsets.all(15.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'Add New Transaction',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      onChanged: (value) {
+                        title = value;
+                      },
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                      onChanged: (value) {
+                        amount = value;
+                      },
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: FlatButton(
+                        padding: EdgeInsets.all(10.0),
+                        color: Colors.green,
+                        onPressed: () {
+                          print(title);
+                          print(amount);
+                        },
+                        child: Text(
+                          'Add',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
             Column(
               children: transactions.map((tx) {
                 return Container(
-                  margin: EdgeInsets.all(5.0),
+                  margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
                   padding: EdgeInsets.all(0.5),
                   decoration: BoxDecoration(
-                    border: Border.all(color: Colors.red, width: 2.0),
+                    border: Border.all(color: Colors.grey, width: 2.0),
                     borderRadius: BorderRadius.all(
                       Radius.circular(15.0),
                     ),
                   ),
-                  child: Card(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Container(
-                          child: Text(
-                            '\$${tx.amount}',
-//                            .toString(),
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                            ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                        child: Text(
+                          '\$${tx.amount}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
                           ),
                         ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(tx.title,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18.0)),
-                            Text(
-                              DateFormat().format(tx.dateTime),
-                              style: TextStyle(color: Colors.grey),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(tx.title,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18.0)),
+                          Text(
+                            DateFormat().format(tx.dateTime),
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      ),
+                    ],
                   ),
                 );
               }).toList(),
