fb19be8cb96c38530e1cc838c06544625a20d34e
mmcguire91 <mmcguire91@gmail.com>
2020-08-27 07:45:11 -0400

    Refactored transaction_list.dart in ListView.builder

--- lib/models/transaction_list.dart
+++ lib/models/transaction_list.dart
@@ -5,58 +5,62 @@ import 'package:intl/intl.dart';
 class TransactionList extends StatelessWidget {
   TransactionList(this.transactions);
   final List<Transaction> transactions;
-  List<Color> _txColors = [Color(0xff01655e), Colors.green];
+  List<Color> _txColors = [Color(0xff228B22), Colors.green];
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Container(
-          margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-          padding: EdgeInsets.all(3.0),
-          decoration: BoxDecoration(
-            gradient: LinearGradient(colors: _txColors),
-            border: Border.all(color: Colors.grey, width: 2.0),
-            borderRadius: BorderRadius.all(
-              Radius.circular(15.0),
-            ),
-            color: Colors.greenAccent,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                child: Text(
-                  '\$${tx.amount}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,
-                    color: Colors.white,
-//                    Color(0xffC5CED4),
-                  ),
-                ),
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Container(
+            margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+            padding: EdgeInsets.all(3.0),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: _txColors),
+              border: Border.all(color: Colors.grey, width: 2.0),
+              borderRadius: BorderRadius.all(
+                Radius.circular(15.0),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    tx.title,
+              color: Colors.greenAccent,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    '\$${transactions[index].amount}',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
-                      fontSize: 18.0,
+                      fontSize: 20.0,
                       color: Colors.white,
+//                    Color(0xffC5CED4),
                     ),
                   ),
-                  Text(
-                    DateFormat().format(tx.dateTime),
-                    style: TextStyle(color: Colors.white70),
-                  )
-                ],
-              ),
-            ],
-          ),
-        );
-      }).toList(),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      DateFormat().format(transactions[index].dateTime),
+                      style: TextStyle(color: Colors.white70),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
