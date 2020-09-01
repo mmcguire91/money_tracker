d4763474c6b2fb1f9fdccf135d9646016a45cec5
mmcguire91 <mmcguire91@gmail.com>
2020-09-01 08:04:17 -0400

    Built modal bottom sheet. Receiving error: Typically, the MediaQuery widget is introduced by the MaterialApp or WidgetsApp widget at the top of your application widget tree.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,60 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
-import 'package:money_tracker/Widgets/user_transactions.dart';
+import 'package:money_tracker/Widgets/new_transaction.dart';
+import './models/transaction_list.dart';
+import './models/transaction.dart';
 
 void main() {
   runApp(Home());
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: '1',
+      title: 'course',
+      amount: 11.99,
+      dateTime: DateTime.now(),
+    ),
+    Transaction(
+      id: '2',
+      title: 'milk',
+      amount: 2.87,
+      dateTime: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double amount) {
+    final newTx = Transaction(
+        title: txTitle,
+        amount: amount,
+        dateTime: DateTime.now(),
+        id: DateTime.now().toString());
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _transactionModal(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,7 +70,10 @@ class Home extends StatelessWidget {
           ),
 //          backgroundColorStart: Color(0xff01655e),
           actions: <Widget>[
-            Icon(Icons.add),
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () => _transactionModal(context),
+            ),
           ],
         ),
         body: SingleChildScrollView(
@@ -40,12 +90,12 @@ class Home extends StatelessWidget {
                   elevation: 5.0,
                 ),
               ),
-              UserTransactions(),
+              TransactionList(_userTransactions),
             ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: null,
+          onPressed: () => _transactionModal(context),
           child: Icon(Icons.add),
           backgroundColor: Color(0xff01655e),
         ),
