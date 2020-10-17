51c90797666332f7e3da618f1ce287744aea24c8
mmcguire91 <mmcguire91@gmail.com>
2020-10-17 15:35:00 -0400

    Added formatting to chart graph

--- lib/Widgets/chart.dart
+++ lib/Widgets/chart.dart
@@ -25,8 +25,8 @@ class Chart extends StatelessWidget {
       }
       //calculate the totalSum according to the time
 
-      print(DateFormat.E().format(weekDay));
-      print(totalSum);
+      // print(DateFormat.E().format(weekDay));
+      // print(totalSum);
 
       return {
         'day': DateFormat.E().format(weekDay).substring(0, 1),
@@ -48,16 +48,23 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6.0,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues.map((data) {
-          return ChartBar(
-            data['day'],
-            data['amount'],
-            totalSpending == 0.0
-                ? 0.0
-                : ((data['amount'] as double) / totalSpending),
-          );
-        }).toList(),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                data['day'],
+                data['amount'],
+                totalSpending == 0.0
+                    ? 0.0
+                    : ((data['amount'] as double) / totalSpending),
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
--- lib/Widgets/chart_bar.dart
+++ lib/Widgets/chart_bar.dart
@@ -11,7 +11,9 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Text('\$${spendingAmount.toStringAsFixed(2)}'),
+        FittedBox(
+          child: Text('\$${spendingAmount.toStringAsFixed(2)}'),
+        ),
         SizedBox(height: 4.0),
         Container(
           height: 60,
